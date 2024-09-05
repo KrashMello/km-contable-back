5c83a67afab8a280195c62f54c9f01e57755ee44
KrashMello <joelenrikesoteldo@gmail.com>
2024-09-05 14:08:30 -0400

    feat: whitelist public

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import modules from "./modules/index";
 import { server } from "elumian/server";
-const whiteList = ["http://localhost:3000"];
+const whiteList = "*";
 server({
   controllers: modules.controllers,
   services: modules.services,
