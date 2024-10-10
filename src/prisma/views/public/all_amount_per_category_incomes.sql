e01313b652d7f9d20b83684ced8224f0c688ecc0
KrashMello <joelenrikesoteldo@gmail.com>
2024-10-10 06:31:05 -0400

    feat: prisma database update and view incomes fix

--- src/prisma/views/public/all_amount_per_category_incomes.sql
+++ src/prisma/views/public/all_amount_per_category_incomes.sql
@@ -1,28 +1,30 @@
-create view vw_all_amount_per_income as SELECT 
-    (SELECT 
-            coalesce(SUM(tt.amount),0)
-        FROM 
-            "Transaction" tt 
-        RIGHT JOIN 
-            Category cc ON cc.id = tt.categoryId 
-        WHERE 
-            cc.transaction_typeId = 1 and
-        cc.name = c.name
-       ) - (
-        SELECT 
-            coalesce(SUM(tt.amount),0)
-        FROM 
-            "Transaction" tt 
-        RIGHT JOIN 
-            Category cc ON cc.id = tt.categoryId 
-        WHERE 
-            cc.transaction_typeId = 2
-         and tt.categoryDebitId = c.id
-    ) AS total_amount,
-    c.name,
-    c.userId
-FROM 
-    "Transaction" AS t 
-RIGHT JOIN 
-    Category c ON c.id = t.categoryId 
-    where c.transaction_typeId = 1
+CREATE VIEW
+  vw_all_amount_per_income as
+SELECT
+  (
+    SELECT
+      coalesce(SUM(tt.amount), 0)
+    FROM
+      "Transaction" tt
+      RIGHT JOIN Category cc ON cc.id = tt.categoryId
+    WHERE
+      cc.transaction_typeId = 1
+      and cc.name = c.name
+  ) - (
+    SELECT
+      coalesce(SUM(tt.amount), 0)
+    FROM
+      "Transaction" tt
+      RIGHT JOIN Category cc ON cc.id = tt.categoryId
+    WHERE
+      cc.transaction_typeId = 2
+      and tt.categoryDebitId = c.id
+  ) AS total_amount,
+  c.name,
+  c.userId
+FROM
+  "Transaction" AS t
+  RIGHT JOIN Category c ON c.id = t.categoryId
+where
+  c.transaction_typeId = 1
+  group by c.accountId
