685e1cd59810f22038d01bf192deddd6435cee10
KrashMello <joelenrikesoteldo@gmail.com>
2025-07-28 12:39:03 -0400

    feat(module): update main

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,18 +1,9 @@
-import { AccountController } from "../controllers/account/account.controller";
-import { AuthController } from "../controllers/auth/auth.controller";
-import { Auth } from "../services/auth.service";
-import { Account } from "../services/Acounts.service";
-import { TransactionController } from "../controllers/transaction/transaction.controller";
-import { Transaction } from "../services/transaction.service";
-import { CategoryController } from "../controllers/category/category.controller";
-import { Category } from "@/services/category.service";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+// import { WalletsModule } from "./wallets/wallets.module";
 
-export default {
-  controllers: [
-    AuthController,
-    AccountController,
-    TransactionController,
-    CategoryController,
-  ],
-  services: [Auth, Account, Transaction, Category],
-};
+export const modules = [
+	UsersModule,
+	// WalletsModule,
+	AuthModule,
+];
