d32d8a24ebced50ffe83db112a1c41954e3526ed
KrashMello <joelenrikesoteldo@gmail.com>
2025-08-01 10:29:55 -0400

    feat: update index modules

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,6 +1,8 @@
 import { AccountTypesModule } from "./account_types/account_types.module";
 import { AuthModule } from "./auth/auth.module";
+import { CategoriesModule } from "./categories/categories.module";
 import { CurrenciesModule } from "./currencies/currencies.module";
+import { TransactionTypesModule } from "./transaction_types/transaction_types.module";
 import { UsersModule } from "./users/users.module";
 import { WalletsModule } from "./wallets/wallets.module";
 
@@ -10,4 +12,6 @@ export const modules = [
 	WalletsModule,
 	CurrenciesModule,
 	AccountTypesModule,
+	CategoriesModule,
+	TransactionTypesModule,
 ];
