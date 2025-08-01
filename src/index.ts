60ce8ebb63616fb6563a8232520a16a158c20d2d
KrashMello <joelenrikesoteldo@gmail.com>
2025-08-01 10:30:12 -0400

    feat: adapter redis provider

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,8 @@ import { modules } from "./modules";
 Server.setConfig({
 	port: 3000,
 });
+Server.setRedisProvider({
+	url: "redis://:@localhost:6379",
+});
 Server.chargeModules(modules);
 Server.start();
