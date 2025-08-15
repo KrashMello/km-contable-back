837f06be5b33d26247ce8a0401abb2d3b646cb38
KrashMello <joelenrikesoteldo@gmail.com>
2025-08-15 09:50:19 -0400

    feat: add modules in index

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,7 +1,10 @@
 import { AccountTypesModule } from "./account_types/account_types.module";
+import { ApiKeysModule } from "./api_keys/api_keys.modules";
 import { AuthModule } from "./auth/auth.module";
 import { CategoriesModule } from "./categories/categories.module";
 import { CurrenciesModule } from "./currencies/currencies.module";
+import { PrismaModule } from "./prisma/prisma.module";
+import { TransactionModule } from "./transaction/transaction.module";
 import { TransactionTypesModule } from "./transaction_types/transaction_types.module";
 import { UsersModule } from "./users/users.module";
 import { WalletsModule } from "./wallets/wallets.module";
@@ -14,4 +17,7 @@ export const modules = [
 	AccountTypesModule,
 	CategoriesModule,
 	TransactionTypesModule,
+	TransactionModule,
+	ApiKeysModule,
+	PrismaModule,
 ];
